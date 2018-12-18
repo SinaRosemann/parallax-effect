ab683a27477855c9e533aee836449a64f3b40016
SinaRosemann <sinarosemann.webdesign@gmail.com>
2018-12-18 14:01:57 +0200

    Update parallax.js

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -26,13 +26,16 @@ function parallax () {
     var moonMove = document.getElementById("moon");
     var moonheight = document.getElementById("section-moon").clientHeight;
     var maxheight = moonheight - 250 + "px";
+    var maxmobile = moonheight - 50 + "px";
     var windowsize = window.innerWidth;
 
 
     moonMove.style.bottom = (window.pageYOffset /8) + 200 + 'px';
 
-    if(moonMove.style.bottom > maxheight && windowsize > 992) {
+    if(moonMove.style.bottom > maxheight && windowsize > 992)) {
         moonMove.style.bottom = maxheight;
+    }else{
+        moonMove.style.bottom = maxmobile;
     }
 
     var textMove = document.getElementById("section-content");
